8413d807bcf2ff5e552d999d566cc3661f1d464d
AazadAhmad <40417095+AazadAhmad@users.noreply.github.com>
2024-07-21 17:59:43 +0530

    Add files via upload

---
+++ site-branding-and-product-launches.php
@@ -0,0 +1,69 @@
+
+    <?php include 'inner-header.php';?>
+    <section class="inner-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="caption text-center position-relative">
+                        <h1 class="wow fadeInDown">Site Branding and <span class="font-color">Product Launches</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="gallery">
+        <div class="container">
+            <div class="row">
+            <div class="col-sm-3">
+                    <div class="gallery-item wow fadeIn">
+                        <img src="images/site-branding/1.jpeg" class="img-fluid" alt="" width="" height="" />
+                        <div class="overlay">
+                            <a href="images/site-branding/1.jpeg" data-lightbox="gallery-1" ><img src="images/loupe.png" width="" height="" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="gallery-item wow fadeIn">
+                        <img src="images/site-branding/2.jpeg" class="img-fluid" alt="" width="" height="" />
+                        <div class="overlay">
+                            <a href="images/site-branding/2.jpeg" data-lightbox="gallery-1" ><img src="images/loupe.png" width="" height="" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="gallery-item wow fadeIn">
+                        <img src="images/site-branding/3.jpeg" class="img-fluid" alt="" width="" height="" />
+                        <div class="overlay">
+                            <a href="images/site-branding/3.jpeg" data-lightbox="gallery-1" ><img src="images/loupe.png" width="" height="" alt=""></a>
+                        </div>
+                    </div>
+                </div>            
+                <div class="col-sm-3">
+                    <div class="gallery-item wow fadeIn">
+                        <img src="images/site-branding/4.jpeg" class="img-fluid" alt="" width="" height="" />
+                        <div class="overlay">
+                            <a href="images/site-branding/4.jpeg" data-lightbox="gallery-1" ><img src="images/loupe.png" width="" height="" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="gallery-item wow fadeIn">
+                        <img src="images/site-branding/5.jpeg" class="img-fluid" alt="" width="" height="" />
+                        <div class="overlay">
+                            <a href="images/site-branding/5.jpeg" data-lightbox="gallery-1" ><img src="images/loupe.png" width="" height="" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="gallery-item wow fadeIn">
+                        <img src="images/site-branding/6.jpeg" class="img-fluid" alt="" width="" height="" />
+                        <div class="overlay">
+                            <a href="images/site-branding/6.jpeg" data-lightbox="gallery-1" ><img src="images/loupe.png" width="" height="" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <?php include 'footer.php';?>
